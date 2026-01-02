aeafe5a01109e527ee1672e24ae606badf238750
zahid-official <zahid.official8@gmail.com>
2026-01-03 05:48:47 +0600

    Problem solving task-2 complete

--- server.ts
+++ server.ts
@@ -1,13 +1,10 @@
 //* 1.Task: Array Filtering and Mapping
-
-// Interface for personsData
 interface IPerson {
   name: string;
   age: number;
   gender: "Male" | "Female";
 }
 
-// Data of people
 const people: IPerson[] = [
   {
     name: "Rahim Ahmed",
@@ -37,7 +34,7 @@ const people: IPerson[] = [
 ];
 
 // getMaleNames function
-const getMaleNames = (peopleData: IPerson[]) => {
+const getMaleNames = (peopleData: IPerson[]): string[] => {
   const filteredData = peopleData.filter(
     (person) => person.gender !== "Female"
   );
@@ -46,4 +43,42 @@ const getMaleNames = (peopleData: IPerson[]) => {
   return result;
 };
 
-console.log(getMaleNames(people));
+// console.log(getMaleNames(people));
+
+//* 2.Task: Object Manipulation
+interface IBook {
+  title: string;
+  author: string;
+  year: number;
+}
+
+const books: IBook[] = [
+  {
+    title: "Pather Panchali",
+    author: "Bibhutibhushan Bandyopadhyay",
+    year: 1929,
+  },
+  {
+    title: "Gitanjali",
+    author: "Rabindranath Tagore",
+    year: 1910,
+  },
+  {
+    title: "Lajja",
+    author: "Taslima Nasrin",
+    year: 1993,
+  },
+  {
+    title: "The Kite Runner",
+    author: "Khaled Hosseini",
+    year: 2003,
+  },
+];
+
+// getBookTitles function
+const getBookTitles = (booksData: IBook[]): string[] => {
+  const result = booksData.map((book) => book.title);
+  return result;
+};
+
+console.log(getBookTitles(books));
