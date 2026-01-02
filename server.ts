43915c2ed7aed83450055fd7064399dd80a89745
zahid-official <zahid.official8@gmail.com>
2026-01-03 05:29:29 +0600

    Problem solving task-1

---
+++ server.ts
@@ -0,0 +1,31 @@
+// 1.Task: Array Filtering and Mapping
+
+const people = [
+  {
+    name: "Rahim Ahmed",
+    age: 30,
+    gender: "Male",
+  },
+  {
+    name: "Ayesha Khan",
+    age: 26,
+    gender: "Female",
+  },
+  {
+    name: "Tanvir Hossain",
+    age: 34,
+    gender: "Male",
+  },
+  {
+    name: "Nusrat Jahan",
+    age: 22,
+    gender: "Female",
+  },
+  {
+    name: "Sajid Hasan",
+    age: 40,
+    gender: "Male",
+  },
+];
+
+
